af0176ca684791373d4e5bc543b9311c58efbae8
Charles <xuchi.int@gmail.com>
2017-07-07 21:24:25 -0700

    modified set-dpdk.sh and ovs-dpdk-start.sh

--- ovs-dpdk-start.sh
+++ ovs-dpdk-start.sh
@@ -4,19 +4,20 @@
 export PATH=$PATH:/usr/local/share/openvswitch/scripts
 export DB_SOCK=/usr/local/var/run/openvswitch/db.sock
 
-# set ovs configurations
-ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
-# multiple pmd threads pinned to CPU cores
-
-ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask=0x6
 
 ovs-ctl --db-sock="$DB_SOCK" start
 
-ovs-vsctl add-br ovsbr0 -- set bridge ovsbr0 datapath_type=netdev
+# set ovs configurations
+ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
 
-ovs-vsctl add-br ovsbr1 -- set bridge ovsbr1 datapath_type=netdev
+# multiple pmd threads pinned to CPU cores
+ovs-vsctl set Open_vSwitch . other_config:pmd-cpu-mask=0x6
 
-ovs-vsctl add-port ovsbr0 enp5s0f0 -- set Interface enp5s0f0 type=dpdk options:dpdk-devargs=0000:06:00.0
+ovs-vsctl --may-exist add-br ovsbr0 -- set bridge ovsbr0 datapath_type=netdev
+ovs-vsctl --may-exist add-br ovsbr1 -- set bridge ovsbr1 datapath_type=netdev
+ovs-vsctl del-port ovsbr0 enp5s0f0
+ovs-vsctl add-port ovsbr0 enp5s0f0 -- set Interface enp5s0f0 type=dpdk options:dpdk-devargs=0000:05:00.0
+ovs-vsctl del-port ovsbr1 enp5s0f1
+ovs-vsctl add-port ovsbr1 enp5s0f1 -- set Interface enp5s0f1 type=dpdk options:dpdk-devargs=0000:05:00.1
 
-ovs-vsctl add-port ovsbr1 enp5s0f1 -- set Interface enp5s0f1 type=dpdk options:dpdk-devargs=0000:06:00.1
-
+ovs-vsctl show
--- set-dpdk.sh
+++ set-dpdk.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+echo '[INFO]: should run as root'
 export DPDK_DIR=/home/chix/dpdk/dpdk-stable-16.11.2
 export DPDK_TARGET=x86_64-native-linuxapp-gcc
 export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
 
-sudo modprobe vfio-pci
+modprobe vfio-pci
+chmod a+x /dev/vfio
+chmod 0666 /dev/vfio/*
+
+
 $DPDK_DIR/tools/dpdk-devbind.py --bind=vfio-pci enp5s0f0
 $DPDK_DIR/tools/dpdk-devbind.py --bind=vfio-pci enp5s0f1
 $DPDK_DIR/tools/dpdk-devbind.py --status
