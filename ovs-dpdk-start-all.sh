32a2bc456dbde16d5f62cc95f9a3197e11e4c9c9
Charles <xuchi.int@gmail.com>
2017-07-10 20:55:50 -0700

    set ip

--- ovs-dpdk-start-all.sh
+++ ovs-dpdk-start-all.sh
@@ -4,3 +4,13 @@
 sudo bash ./set-dpdk.sh
 sleep 1
 sudo bash ./ovs-dpdk-start.sh
+sleep 1
+#set ovs-br-ip
+#not included in git repo
+#example:
+#sudo ifconfig ovsbr0 192.168.100.102
+#sudo ifconfig ovsbr0 up
+#sudo ifconfig ovsbr1 192.168.101.102
+#sudo ifconfig ovsbr1 up
+sudo bash ./set-ovsbr-ip.sh
+
