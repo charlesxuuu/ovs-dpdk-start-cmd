131d7b1fec18b2d47494f8fabbd7505e03b0a2d2
Charles <xuchi.int@gmail.com>
2017-07-10 19:43:31 -0700

    update

--- set-dpdk.sh
+++ set-dpdk.sh
@@ -5,8 +5,11 @@ export DPDK_DIR=/home/chix/dpdk/dpdk-stable-16.11.2
 export DPDK_TARGET=x86_64-native-linuxapp-gcc
 export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
 
+mkdir -p /mnt/huge
+mkdir -p /mnt/huge_2mb
 
-mount -t hugetlbfs -o pagesize=1G none /dev/hugepages
+mount -t hugetlbfs hugetlbfs /mnt/huge
+mount -t hugetlbfs none /mnt/huge_2mb -o pagesize=2MB
 
 modprobe vfio-pci
 chmod a+x /dev/vfio
