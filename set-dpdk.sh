ca22d6e54988d7e5ec58ce8e9bfa31fb371a00ac
Charles <xuchi.int@gmail.com>
2017-07-12 15:47:03 -0700

    update

--- set-dpdk.sh
+++ set-dpdk.sh
@@ -6,14 +6,17 @@ export DPDK_TARGET=x86_64-native-linuxapp-gcc
 export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
 
 mkdir -p /mnt/huge
-
+sysctl -w vm.nr_hugepages=4096
 mount -t hugetlbfs hugetlbfs /mnt/huge
 
 modprobe vfio-pci
+modprobe uio_pci_generic
 chmod a+x /dev/vfio
 chmod 0666 /dev/vfio/*
 
 
 $DPDK_DIR/tools/dpdk-devbind.py --bind=vfio-pci enp5s0f0
 $DPDK_DIR/tools/dpdk-devbind.py --bind=vfio-pci enp5s0f1
+#$DPDK_DIR/tools/dpdk-devbind.py --bind=uio_pci_generic enp5s0f0
+#$DPDK_DIR/tools/dpdk-devbind.py --bind=uio_pci_generic enp5s0f1
 $DPDK_DIR/tools/dpdk-devbind.py --status
