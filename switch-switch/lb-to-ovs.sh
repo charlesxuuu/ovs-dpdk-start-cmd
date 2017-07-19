f85a929207bc47ff8bca175290248702cc7b143a
Charles <xuchi.int@gmail.com>
2017-07-18 17:27:41 -0700

    update

---
+++ switch-switch/lb-to-ovs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# get ports and cache
+sudo ovs-vsctl show |grep 'ovsbr0\|ovsbr1'
+sudo brctl show |grep 'lbbr0\|lbbr1'
+sudo brctl addbr lbbr0
+sudo brctl addbr lbbr1
+
+#sudo ovs-vsctl list-ports ovsbr0 > ports-on-ovsbr0
+#sudo ovs-vsctl list-ports ovsbr1 > ports-on-ovsbr1
+
+filename="ports-on-ovsbr0"
+
+while read -r line
+do
+	portname="$line"
+	brctl delif lbbr0 $portname
+	ovs-vsctl add-port ovsbr0 $portname
+done < "$filename"
+
+filename="ports-on-ovsbr1"
+
+while read -r line
+do
+        portname="$line"
+        brctl delif lbbr1 $portname
+        ovs-vsctl add-port ovsbr1 $portname
+done < "$filename"
+
+ifconfig lbbr0 0.0.0.0
+ifconfig lbbr1 0.0.0.0
+ifconfig ovsbr0 192.168.100.102
+ifconfig ovsbr1 192.168.101.102
+ifconfig ovsbr0 up
+ifconfig ovsbr1 up
---
+++ switch-switch/ovs-to-lb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# get ports and cache
+sudo ovs-vsctl show |grep 'ovsbr0\|ovsbr1'
+sudo brctl show |grep 'lbbr0\|lbbr1'
+sudo brctl addbr lbbr0
+sudo brctl addbr lbbr1
+
+#sudo ovs-vsctl list-ports ovsbr0 > ports-on-ovsbr0
+#sudo ovs-vsctl list-ports ovsbr1 > ports-on-ovsbr1
+
+filename="ports-on-ovsbr0"
+
+while read -r line
+do
+	portname="$line"
+	echo "$portname"
+	ovs-vsctl del-port $portname
+	brctl addif lbbr0 $portname
+done < "$filename"
+
+filename="ports-on-ovsbr1"
+
+while read -r line
+do
+        portname="$line"
+        ovs-vsctl del-port $portname
+        brctl addif lbbr1 $portname
+done < "$filename"
+
+ifconfig ovsbr0 0.0.0.0
+ifconfig ovsbr1 0.0.0.0
+ifconfig lbbr0 192.168.100.102
+ifconfig lbbr1 192.168.101.102
+ifconfig lbbr0 up
+ifconfig lbbr1 up
